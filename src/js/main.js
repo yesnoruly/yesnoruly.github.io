3b53ad57825515906656cb85b6d1fcf3f49e4bc8
yesnoruly <karvatskij.roma@gmail.com>
2023-01-24 12:44:52 +0200

    do responsive

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,30 @@ for (let i = 0; i < card.length; i++) { // Prevent adding a class to all cards w
     card[i].addEventListener('mouseenter', () => {
         card[i].classList.add('card--visible')
     })
-    
+
     card[i].addEventListener('mouseleave', () => {
         card[i].classList.remove('card--visible')
+    })
+}
+
+// Burger menu
+
+const headerList = document.querySelector('.header__list');
+const headerItem = document.querySelectorAll('.header__item')
+const headerBurger = document.querySelector('.header__burger');
+const body = document.querySelector('body');
+
+headerBurger.addEventListener('click', (e) => {
+    e.preventDefault();
+    headerList.classList.toggle('header--active');
+    headerBurger.classList.toggle('header--active');
+    body.classList.toggle('lock');
+})
+
+for (let i = 0; i < headerItem.length; i++) {
+    headerItem[i].addEventListener('click', () => {
+        headerList.classList.remove('header--active');
+        headerBurger.classList.remove('header--active');
+        body.classList.remove('lock');
     })
 }
