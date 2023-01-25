ef17faac022e6cd313b0165733dff59c43c5e19a
yesnoruly <karvatskij.roma@gmail.com>
2023-01-25 11:14:55 +0200

    on small resolutions disable card hover

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,19 @@
 // Card Hover 
 
+const mq = window.matchMedia("(min-width: 768px)");
 const card = document.querySelectorAll('.card')
 
-for (let i = 0; i < card.length; i++) { // Prevent adding a class to all cards when hover
-    card[i].addEventListener('mouseenter', () => {
-        card[i].classList.add('card--visible')
-    })
+if (mq.matches) {
+    console.log(card)
+    for (let i = 0; i < card.length; i++) { // Prevent adding a class to all cards when hover
+        card[i].addEventListener('mouseenter', () => {
+            card[i].classList.add('card--visible')
+        })
 
-    card[i].addEventListener('mouseleave', () => {
-        card[i].classList.remove('card--visible')
-    })
+        card[i].addEventListener('mouseleave', () => {
+            card[i].classList.remove('card--visible')
+        })
+    }
 }
 
 // Burger menu
