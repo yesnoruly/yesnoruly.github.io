f9cf07993727bd300b7b35dd335b0475c2344883
yesnoruly <karvatskij.roma@gmail.com>
2023-01-29 10:28:30 +0200

    replace menu link click sound

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -6,8 +6,8 @@ const headerBurger = document.querySelector('.header__burger');
 const body = document.querySelector('body');
 // Assets
 const close = new Audio('https://user-images.githubusercontent.com/64963734/214936574-7376fc2a-aa12-432e-9f01-cc1155121366.mp4')
-const open = new Audio('https://user-images.githubusercontent.com/64963734/214936588-c9b89274-138f-430b-83c4-26428576a369.mp4')
-const menu_hover = new Audio('https://user-images.githubusercontent.com/64963734/215226583-f3229be2-b3e4-48d8-af93-9bfd88aa6243.mp4')
+const open = new Audio('https://user-images.githubusercontent.com/64963734/215313590-ad04d43f-e6bc-4c13-9a1c-21169cc59fbb.mp4')
+const link = new Audio('https://user-images.githubusercontent.com/64963734/215226583-f3229be2-b3e4-48d8-af93-9bfd88aa6243.mp4')
 
 headerBurger.addEventListener('click', (e) => {
     e.preventDefault();
@@ -23,13 +23,12 @@ headerBurger.addEventListener('click', (e) => {
     }
 })
 
-// Close when click on link ----------------------
-for (let i = 0; i < headerItem.length; i++) {
+for (let i = 0; i < headerItem.length; i++) { // Close menu when click
     headerItem[i].addEventListener('click', () => {
         headerList.classList.remove('header--active');
         headerBurger.classList.remove('header--active');
         body.classList.remove('lock');
         
-        playSound(menu_hover)
+        playSound(link)
     })
 }
