8f2dfb45bdae1ed2d37463fd955e5e4198c9e9fc
yesnoruly <karvatskij.roma@gmail.com>
2023-01-29 12:02:48 +0200

    sound disable animation

--- src/js/sound.js
+++ src/js/sound.js
@@ -1,24 +1,24 @@
-const headerSound = document.querySelector('.header__sound');
+const sound = document.querySelector('.sound');
 const contactIcons = document.querySelectorAll('.contact__icon')
 // Audio assets
 const disable_sound = new Audio('https://user-images.githubusercontent.com/64963734/214936578-0008b044-447f-4742-b3bd-1aab2636830c.mp4')
 const enable_sound = new Audio('https://user-images.githubusercontent.com/64963734/214936581-e361bf17-7bbe-45d6-92d1-f94bfc3c6e44.mp4')
 const card_hover = new Audio('https://user-images.githubusercontent.com/64963734/215278132-6c65d3ee-018b-4125-b26c-d49cdd8d79fc.mp4')
 
-headerSound.addEventListener('click', () => {
-    headerSound.classList.toggle('header__sound--disable')
+sound.addEventListener('click', () => {
+    sound.classList.toggle('sound--disable')
 
-    if (headerSound.classList.contains('header__sound--disable')) {
+    if (sound.classList.contains('sound--disable')) {
         disable_sound.play()
     } else {
         enable_sound.play()
     }
 })
 
-export function playSound(sound) {
-    if (headerSound.classList.contains('header__sound--disable')) return
+export function playSound(src) {
+    if (sound.classList.contains('sound--disable')) return
 
-    sound.play()
+    src.play()
 }
 
 // ----------------------------------------------
