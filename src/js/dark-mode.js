bc5c8c35c49f60a10ec8269926e40e20a9c960b9
yesnoruly <karvatskij.roma@gmail.com>
2023-01-30 06:17:05 +0200

    refactor user handling mode

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,7 +1,7 @@
 import { playSound } from "./sound";
 
 const themeSwitch = document.querySelector('.header__switch');
-
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
 // Sound
 const switch_off = new Audio('https://user-images.githubusercontent.com/64963734/215277655-2a6fe4fa-935c-4087-aef5-9170c6789067.mp4')
 const switch_on = new Audio('https://user-images.githubusercontent.com/64963734/215277657-f068ca6a-88ed-463e-8cb0-b9f1136ab60f.mp4')
@@ -16,21 +16,30 @@ themeSwitch.addEventListener('click', () => {
     }
 });
 
-const currentTheme = localStorage.getItem('theme')
-const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+let isDark = false;
 
-if (currentTheme === 'dark') {
-    document.documentElement.classList.add('dark')
+// default to system setting
+if (prefersDarkScheme.matches) {
+    isDark = true;
 }
 
-themeSwitch.addEventListener('click', () => {
+// preference from localStorage should overwrite
+if (localStorage.getItem('theme') === 'dark') {
+    isDark = true;
+} else if (localStorage.getItem('theme') === 'light') {
+    isDark = false;
+}
+
+if (isDark) {
     document.documentElement.classList.toggle('dark');
+}
 
-    let theme = '';
+document.addEventListener('DOMContentLoaded', () => {
 
-    if (prefersDarkTheme.matches) {
-        currentTheme === '' ? theme = 'dark' : null
-    }
+    themeSwitch.addEventListener('click', () => {
+        
+        document.documentElement.classList.toggle('dark');
+        localStorage.setItem('theme', document.documentElement.classList.contains('dark') ? 'dark' : 'light');
+    });
 
-    localStorage.setItem('theme', theme)
-})
+});
